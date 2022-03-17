918c12049e70469f794a6112d835ba4bb3cf103a
sebjvidal <seb.vidal@btinternet.com>
2022-03-17 18:32:06 +0000

    Initial Commit

---
+++ Sizing Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sizing Test
+//
+//  Created by Seb Vidal on 17/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
